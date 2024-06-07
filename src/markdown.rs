b9d6fe92f46c0d8080615e186cf6633cbdc088d9
hkau <no_reply_in@sentrytwo.com>
2024-06-07 18:10:40 -0400

    add: "shared" -> "sauropod"

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,4 +1,4 @@
-use shared::markdown::parse_markdown as shared_parse_markdown;
+use sauropod::markdown::parse_markdown as shared_parse_markdown;
 
 pub fn parse_markdown(original_in: String) -> String {
     shared_parse_markdown(original_in, Vec::new())
